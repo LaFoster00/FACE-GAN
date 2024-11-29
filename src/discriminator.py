98cdfc4c4c95e177000432f1e192a995f4c5aa0f
Lasse Foster <lasse.foster@web.de>
2024-11-29 20:02:16 +0100

    * theoretically fully implemented discriminator base on progan

--- src/discriminator.py
+++ src/discriminator.py
@@ -1,66 +1,99 @@
-from keras import models, layers, models, ops
+from keras import models, layers, models, ops, backend
 import keras
+
 from layers import WeightedAdd
+import numpy as np
 
+from layers import FromRGB, PixelNorm
 
-
-def conv_block(
-        x,
-        filters,
-        activation=layers.LeakyReLU(negative_slope=0.2),
-        kernel_size=(3, 3),
-        strides=(1, 1),
-        padding="same",
-        use_bn=False,
-        use_dropout=False,
-        drop_value=0.5,
-):
-    x = layers.Conv2D(filters, kernel_size, strides=strides, padding=padding)(x)
-    if use_bn:
-        x = layers.BatchNormalization()(x)
-    x = activation(x)
-    return x
+from utils import number_features, lerp_clip, cset, lerp
 
 
 def get_discriminator_model(
-        num_color_channels=3,
-        start_filters=64,
-        max_filters=512,
-        target_resolution=256,
+    images_in,                          # Input: Images [minibatch, channel, height, width].
+    num_channels        = 1,            # Number of input color channels. Overridden based on dataset.
+    resolution          = 32,           # Input resolution. Overridden based on dataset.
+    label_size          = 0,            # Dimensionality of the labels, 0 if no labels. Overridden based on dataset.
+    fmap_base           = 8192,         # Overall multiplier for the number of feature maps.
+    fmap_decay          = 1.0,          # log2 feature map reduction when doubling the resolution.
+    fmap_max            = 512,          # Maximum number of feature maps in any layer.
+    structure           = None,         # 'linear' = human-readable, 'recursive' = efficient, None = select automatically
+    is_template_graph   = False,        # True = template graph constructed by the Network class, False = actual evaluation.
 ):
-    """
-    Create a progressive discriminator model with multiple resolution scales
-    :return:
-        List of discriminator models at different scales
-    """
-    num_blocks = ops.log2(target_resolution / 4)
+    resolution_log2 = int(np.log2(resolution))
+    assert resolution == 2**resolution_log2 and resolution >= 4
+    def nf(log2res):
+        return number_features(log2res, fmap_base, fmap_decay, fmap_max)
+    def downscale2D(x, factor=2):
+        # TODO check if strides is correct it might need to be (1,1)
+        return layers.AveragePooling2D(pool_size=(factor, factor), strides=(factor, factor), padding='valid',data_format='channel_first')(x)
+    def fromrgb(x, log2res):
+        return FromRGB(log2res, fmap_base, fmap_decay, fmap_max)(x)
+    if structure is None: structure = 'linear' if is_template_graph else 'recursive'
 
-    discriminator_models = []
+    lod_in = ops.cast(keras.Variable('lod', initializer=np.float32(0.0), trainable=False), backend.floatx())
 
-    #Start with 4x4 resolution discriminator
-    img_input = layers.Input(shape=(4, 4, num_color_channels))
-    x = conv_block(img_input, start_filters)
-    x = layers.Flatten()(x)
-    x = layers.Dense(64, activation='sigmoid')(x)
+    def discriminator_block(x, log2res):
+        if log2res >= 3: # 8x8 and up
+            x = layers.Conv2D(
+                filters=nf(log2res - 1),
+                kernel_size=(3, 3),
+                strides=(1, 1),
+                padding='same',
+                data_format='channels_first')(x)
+            x = layers.LeakyReLU(negative_slope=0.2)(x)
+            x = layers.Conv2D(
+                filters=nf(log2res - 2),
+                kernel_size=(3, 3),
+                strides=(1, 1),
+                padding='same',
+                data_format='channels_first')(x)
+            x = layers.LeakyReLU(negative_slope=0.2)(x)
+            x = downscale2D(x)
+        else: # 4x4
+            x = layers.Conv2D(
+                filters=nf(log2res - 1),
+                kernel_size=(3, 3),
+                strides=(1, 1),
+                padding='same',
+                data_format='channels_first')(x)
+            x = layers.LeakyReLU(negative_slope=0.2)(x)
+            x = layers.Conv2D(
+                filters=nf(log2res - 2),
+                kernel_size=(3, 3),
+                strides=(1, 1),
+                padding='same',
+                data_format='channels_first')(x)
+            x = layers.LeakyReLU(negative_slope=0.2)(x)
+            x = layers.Dense(1+label_size)(x)
+        return x
 
-    discriminator_models.append(models.Model(img_input, x, name='discriminator_4x4'))
+    # Linear structure: simple but inefficient
+    if structure == 'linear':
+        img = images_in
+        x = fromrgb(img, resolution_log2)
+        for res in range(resolution_log2, 2, -1):
+            lod = resolution_log2 - res
+            x = discriminator_block(x, res)
+            img = downscale2D(img)
+            y = fromrgb(img, res - 1)
+            x = lerp_clip(x, y, lod_in - lod)
+        combo_out = discriminator_block(x, 2)
 
-    #Progessive growth of discriminator
-    current_filters = start_filters
-    current_resolution = 4
+    # Recursive structure: complex but efficient.
+    if structure == 'recursive':
+        def grow(res, lod):
+            x = lambda: fromrgb(downscale2D(img, 2**lod), res)
+            if lod > 0: x = cset(x, (lod_in < lod), lambda: grow(res + 1, lod - 1))
+            x = discriminator_block(x(), res); y = lambda: x
+            if res > 2: y = cset(y, (lod_in > lod), lambda: lerp(x, fromrgb(downscale2D(images_in, 2**(lod+1)), res - 1), lod_in - lod))
+            return y()
+        combo_out = grow(2, resolution_log2 - 2)
 
-    # Create the next downscale steps
-    for i in range(num_blocks):
-        current_resolution *= 2
-        current_filters = min(current_filters * 2, max_filters)
+    scores_out = layers.Identity(name='score_out')(combo_out[:, :1])
+    labels_out = layers.Identity(name='labels_out')(combo_out[:, 1:])
+    return scores_out, labels_out
 
-        # New higher resolution input
-        img_input = layers.Input(shape=(current_resolution, current_resolution, num_color_channels))
-        #Downsample input
-        x = layers.AveragePooling2D()(img_input)
-        x = conv_block(x, current_filters // 2)
-        x = conv_block(x, current_filters, strides=(2, 2))
-        x = conv_block(x, current_filters)
 
 
 
